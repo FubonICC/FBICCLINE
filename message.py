1c10986b0b4aeec1cfc0e49d17b920d53081178d
CHIU-Wei-Lun <m10975713@mail.nknu.edu.tw>
2022-03-10 00:11:29 +0800

    ok

--- message.py
+++ message.py
@@ -237,6 +237,75 @@ def Carousel_Template2():
     )
     return message
 
+def Carousel_Template3():
+    message = TemplateSendMessage(
+        alt_text='海軍通訊系統指揮部',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://navy.mnd.gov.tw/Photo/PartnerHTML/201612211656_260768.png',
+                    title='陸戰九九旅',
+                    text='鐵軍：鋼鐵勁旅，防衛固守，有如銅牆鐵壁之堅強勁旅',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='駐地：林園',
+                            data='將這個訊息偷偷回傳給機器人'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是1'
+                        ),
+                        URITemplateAction(
+                            label='了解陸戰九九旅',
+                            uri='https://navy.mnd.gov.tw/AboutUs/Partner_Info.aspx?ID=30151&AID=30329'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
+                    title='這是第二塊模板',
+                    text='副標題可以自己改',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='回傳一個訊息',
+                            data='這是ID=2'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是2'
+                        ),
+                        URITemplateAction(
+                            label='進入2的網頁',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Number_2_in_light_blue_rounded_square.svg/200px-Number_2_in_light_blue_rounded_square.svg.png'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
+                    title='這是第三個模塊',
+                    text='最多可以放十個',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='回傳一個訊息',
+                            data='這是ID=3'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是3'
+                        ),
+                        URITemplateAction(
+                            label='uri2',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    return message
+
+
+
 
 
 #TemplateSendMessage - ImageCarouselTemplate(圖片旋轉木馬)
