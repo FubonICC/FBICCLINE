3974956d32796ebdccf7dbb7bd563cb21bb4235c
CHIU-Wei-Lun <m10975713@mail.nknu.edu.tw>
2022-03-09 22:08:15 +0800

    ok

--- app.py
+++ app.py
@@ -48,6 +48,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    message =  textsendmessage(text="你說的是不是:" + event.message.text )
+    line_bot_api.reply_message(event.reply_token, message)
     msg = event.message.text
     if '最新合作廠商' in msg:
         message = imagemap_message()
