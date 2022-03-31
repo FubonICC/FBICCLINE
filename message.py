9c2888a03f0fb2257d5d326c322086c485a63bb3
CHIU-Wei-Lun <m10975713@mail.nknu.edu.tw>
2022-04-01 02:03:30 +0800

    ok

--- message.py
+++ message.py
@@ -8,43 +8,36 @@ def imagemap_message():
     message = ImagemapSendMessage(
         base_url="https://static.wixstatic.com/media/f96bc7_e631f625a59c4bccabbadad6d6cd0ecf~mv2.jpg/v1/fill/w_357,h_228,al_c,q_80,usm_0.66_1.00_0.01/recuitmenu01.webp",
         alt_text='最新的合作廠商有誰呢？',
-        base_size=BaseSize(height=2000, width=2000),
+        base_size=BaseSize(height=228, width=356),
         actions=[
             URIImagemapAction(
                 #家樂福
                 link_uri="https://tw.shop.com/search/%E5%AE%B6%E6%A8%82%E7%A6%8F",
                 area=ImagemapArea(
-                    x=0, y=0, width=1000, height=1000
+                    x=0, y=0, width=148, height=114
                 )
             ),
             URIImagemapAction(
                 #生活市集
                 link_uri="https://tw.shop.com/search/%E7%94%9F%E6%B4%BB%E5%B8%82%E9%9B%86",
                 area=ImagemapArea(
-                    x=1000, y=0, width=1000, height=1000
+                    x=148, y=0, width=148, height=114
                 )
             ),
             URIImagemapAction(
                 #阿瘦皮鞋
                 link_uri="https://tw.shop.com/search/%E9%98%BF%E7%98%A6%E7%9A%AE%E9%9E%8B",
                 area=ImagemapArea(
-                    x=0, y=1000, width=1000, height=1000
+                    x=0, y=114, width=148, height=114
                 )
             ),
             URIImagemapAction(
                 #塔吉特千層蛋糕
                 link_uri="https://tw.shop.com/search/%E5%A1%94%E5%90%89%E7%89%B9",
                 area=ImagemapArea(
-                    x=1000, y=1000, width=1000, height=500
+                    x=148, y=114, width=148, height=114
                 )
             ),
-            URIImagemapAction(
-                #亞尼克生乳捲
-                link_uri="https://tw.shop.com/search/%E4%BA%9E%E5%B0%BC%E5%85%8B",
-                area=ImagemapArea(
-                    x=1000, y=1500, width=1000, height=500
-                )
-            )
         ]
     )
     return message
