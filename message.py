b9254f0d366f191029670371536f49fe9ec87f37
CHIU-Wei-Lun <m10975713@mail.nknu.edu.tw>
2022-04-01 02:12:24 +0800

    ok

--- message.py
+++ message.py
@@ -8,34 +8,34 @@ def imagemap_message():
     message = ImagemapSendMessage(
         base_url="https://static.wixstatic.com/media/f96bc7_0eb890e5dd494e889bab0191d7aff419~mv2.jpg/v1/fill/w_600,h_383,al_c,q_80,usm_0.66_1.00_0.01/recruitmenu02.webp",
         alt_text='最新的合作廠商有誰呢？',
-        base_size=BaseSize(height=228, width=356),
+        base_size=BaseSize(height=912, width=1428),
         actions=[
             URIImagemapAction(
                 #家樂福
                 link_uri="https://tw.shop.com/search/%E5%AE%B6%E6%A8%82%E7%A6%8F",
                 area=ImagemapArea(
-                    x=0, y=0, width=148, height=114
+                    x=0, y=0, width=714, height=456
                 )
             ),
             URIImagemapAction(
                 #生活市集
                 link_uri="https://tw.shop.com/search/%E7%94%9F%E6%B4%BB%E5%B8%82%E9%9B%86",
                 area=ImagemapArea(
-                    x=148, y=0, width=148, height=114
+                    x=714, y=0, width=714, height=456
                 )
             ),
             URIImagemapAction(
                 #阿瘦皮鞋
                 link_uri="https://tw.shop.com/search/%E9%98%BF%E7%98%A6%E7%9A%AE%E9%9E%8B",
                 area=ImagemapArea(
-                    x=0, y=114, width=148, height=114
+                    x=0, y=456, width=714, height=456
                 )
             ),
             URIImagemapAction(
                 #塔吉特千層蛋糕
                 link_uri="https://tw.shop.com/search/%E5%A1%94%E5%90%89%E7%89%B9",
                 area=ImagemapArea(
-                    x=148, y=114, width=148, height=114
+                    x=714, y=456, width=714, height=456
                 )
             ),
         ]
