7820fe9481994676fc03cdbe7d35045f0f0dbeaf
CHIU-Wei-Lun <m10975713@mail.nknu.edu.tw>
2022-04-12 17:38:43 +0800

    ok

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from linebot.models import TextSendMessage, ImageSendMessage
 from message import *
 from new import *
 from Function import *
+from pictures import *
 #======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
