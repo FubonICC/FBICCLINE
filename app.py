23b5081a97462c21a6ebf2d2c1fc0a72e0dbd32a
CHIU-Wei-Lun <m10975713@mail.nknu.edu.tw>
2022-10-24 02:33:45 +0800

    ok

--- app.py
+++ app.py
@@ -239,6 +239,9 @@ def handle_message(event):
     elif '志兵正期比較圖'in msg:
         message = ImageSendMessage(original_content_url="https://i.imgur.com/n1Tj4xQ.jpg", preview_image_url="https://i.imgur.com/n1Tj4xQ.jpg")
         line_bot_api.reply_message(event.reply_token, message)
+    elif '112梯次'in msg:
+        message = ImageSendMessage(original_content_url="https://i.imgur.com/wzszzFx.jpg", preview_image_url="https://i.imgur.com/wzszzFx.jpg")
+        line_bot_api.reply_message(event.reply_token, message)        
     elif '小編'in msg:
         message = ImageSendMessage(original_content_url="https://i.imgur.com/evPGRYz.jpg", preview_image_url="https://i.imgur.com/evPGRYz.jpg")
         line_bot_api.reply_message(event.reply_token, message)
